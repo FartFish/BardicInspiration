537c163127aa505e1c526b1691fe077c8a3bd005
FartFish <adamshai5457@gmail.com>
2024-10-25 18:09:42 -0700

    object display movement updated

--- scenes/game_object_display.gd
+++ scenes/game_object_display.gd
@@ -2,12 +2,16 @@ extends Node2D
 
 @export var game_object:GameObject
 
-var pos_tween_str:float = 4.0
+var pos_tween_str:float = 8.0
 
 
 
 func _process(delta):
 	if game_object:
 		var tile_size = GameManager.displayed_tile_size
-		position += (Vector2(game_object.as_vector() * tile_size + Vector2i(tile_size / 2, tile_size / 2) ) - position) * delta * pos_tween_str
+		var targ_pos = Vector2(game_object.as_vector() * tile_size + Vector2i(tile_size / 2, tile_size / 2) )
+		position += (targ_pos - position) * delta * pos_tween_str
+		
+		if (targ_pos - position).length() < 0.5:
+			position = targ_pos
 		
