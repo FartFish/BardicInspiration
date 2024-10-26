57eec73d4646a9036cb23ca9c51486c518696c82
FartFish <adamshai5457@gmail.com>
2024-10-25 17:51:47 -0700

    pos tweening update

--- scenes/game_object_display.gd
+++ scenes/game_object_display.gd
@@ -2,12 +2,12 @@ extends Node2D
 
 @export var game_object:GameObject
 
-var pos_tween_str:float = 1.0
+var pos_tween_str:float = 4.0
 
 
 
 func _process(delta):
 	if game_object:
 		var tile_size = GameManager.displayed_tile_size
-		position += (Vector2(game_object.as_vector() * tile_size + Vector2i(tile_size, tile_size) ) - position) * delta * pos_tween_str
+		position += (Vector2(game_object.as_vector() * tile_size + Vector2i(tile_size / 2, tile_size / 2) ) - position) * delta * pos_tween_str
 		
